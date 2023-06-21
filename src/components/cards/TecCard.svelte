7e7d862d0a2f2f45a2e042389f9d6ea15f3c5f3a
Matheus Marques <matheus05linhares@hotmail.com>
2023-06-21 17:28:14 -0300

    .

---
+++ src/components/cards/TecCard.svelte
@@ -0,0 +1,23 @@
+<i class="fa-brands fa-docker"></i>
+<i class="fa-brands fa-square-js"></i>
+<i class="fa-brands fa-node-js"></i>
+<i class="fa-brands fa-node"></i>
+<i class="fa-brands fa-vuejs"></i>
+<i class="fa-brands fa-php"></i>
+<i class="fa-brands fa-python"></i>
+<i class="fa-brands fa-react"></i>
+<i class="fa-brands fa-angular"></i>
+<i class="fa-solid fa-webhook"></i>
+<i class="fa-brands fa-html5"></i>
+<i class="fa-brands fa-sass"></i>
+<i class="fa-brands fa-css3-alt"></i>
+<i class="fa-solid fa-database"></i>
+
+cakephp
+express
+svelte
+websocket
+tailwind
+flex-box
+mysql
+class:name}
