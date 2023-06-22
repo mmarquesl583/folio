8f9d40b354305cf4a5dc11c792d99faa624f45b9
matheusml583 <matheus05linhares@hotmail.com>
2023-06-22 14:32:50 -0300

    .

--- src/pages/Tecnologies.svelte
+++ src/pages/Tecnologies.svelte
@@ -8,7 +8,7 @@
             TECNOLOGIAS
         </span>
         <div class="mt-8 grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-            <span class="col-span-2 sm:col-span-3 md:col-span-4 lg:col-span-5 text-xl lg:text-3xl">Domino:</span>
+            <span class="col-span-2 sm:col-span-3 md:col-span-4 lg:col-span-5 text-2xl lg:text-3xl">Domino:</span>
             <TecCard border_color="gold" color="yellow" i_class="fa-brands fa-square-js" />
             <TecCard border_color="gold" color="green" i_class="fa-brands fa-node" />
             <TecCard border_color="gold" color="orange" text="Svelte" />
@@ -25,14 +25,14 @@
             <TecCard border_color="gold" color="red" i_class="fa-brands fa-html5" />
             <TecCard border_color="gold" color="blue" i_class="fa-brands fa-css3-alt" />
 
-            <span class="col-span-2 sm:col-span-3 md:col-span-4 lg:col-span-5 text-xl lg:text-3xl">Conhecimentos Gerais:</span>
+            <span class="col-span-2 sm:col-span-3 md:col-span-4 lg:col-span-5 text-2xl lg:text-3xl text-gray-200">Conhecimentos Gerais:</span>
             <TecCard border_color="silver" color="red" text="CakePHP" />
             <TecCard border_color="silver" color="orange" i_class="fa-brands fa-python" />
             <TecCard border_color="silver" color="red" i_class="fa-brands fa-angular" />
 
-            <span class="col-span-2 sm:col-span-3 md:col-span-4 lg:col-span-5 text-xl lg:text-3xl">Aprendendo:</span>
-            <TecCard border_color="silver" color="blue" i_class="fa-brands fa-react" />
-            <TecCard border_color="silver" color="green" text="Express" />
+            <span class="col-span-2 sm:col-span-3 md:col-span-4 lg:col-span-5 text-2xl lg:text-3xl text-gray-5000">Estudando:</span>
+            <TecCard border_color="bronze" color="blue" i_class="fa-brands fa-react" />
+            <TecCard border_color="bronze" color="green" text="Express" />
         </div>
 
     </div>
